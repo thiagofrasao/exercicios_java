17dd5919b591992f86a1983640dc6c892eb9a830
Thiago Frasão <thiagofrasao@gmail.com>
2021-06-15 12:12:50 -0300

    exercicio_resolvido_2

---
+++ exercicio_resolvido_2/src/exercicio_resolvido_2.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+	
+public class exercicio_resolvido_2 {
+		
+	public static void main (String[]args) {
+			
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+			
+		double base = sc.nextDouble();
+		double altura = sc.nextDouble();
+			
+		double area = base * altura;
+		double perimetro = 2.0 * (base + altura);
+		double diagonal = Math.sqrt(Math.pow(base, 2.0) + Math.pow(altura, 2.0));
+			 
+		System.out.printf("AREA = %.4f%n", area);
+		System.out.printf("PERÍMETRO = %.4f%n", perimetro);
+		System.out.printf("DIAGONAL = %.4f%n", diagonal);
+			
+		sc.close();
+	}
+}
+			
